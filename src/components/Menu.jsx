e137f36bd3cc8a0746e02b70993a8f5a3d2cd4f4
Carlos Perren <idux.net@gmail.com>
2023-09-04 18:42:55 -0300

    Actualización menu y estilos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,22 +15,22 @@ const Menu = () => {
   };
 
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" style={{zIndex: 40000}}>
       <Container>
         <Link className="navbar-brand" to="/">GiftCards Shop</Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto" id="menu">
-            <NavLink to="/" className={({isActive}) => isActive ? 'nav-item nav-link active': 'nav-item nav-link'}>Home</NavLink>
-            <NavLink to="/giftcards" className={({isActive}) => isActive ? 'nav-item nav-link active': 'nav-item nav-link'}>GiftCards</NavLink>
+            <NavLink to="/" className={({isActive}) => isActive ? 'nav-item nav-link active': 'nav-item nav-link'} style={{textAlign: 'center'}}>Home</NavLink>
+            <NavLink to="/giftcards" className={({isActive}) => isActive ? 'nav-item nav-link active': 'nav-item nav-link'} style={{textAlign: 'center'}}>GiftCards</NavLink>
           </Nav>
         </Navbar.Collapse>
         
         <Navbar.Collapse className=" justify-content-end">
           <Nav>
             {user ?
-              <Button variant="warning" onClick={logOut}><i className="fa fa-sign-out me-1"></i>{user.username}</Button>
+              <Button variant="warning" onClick={logOut}>{user.name} ({user.role})&nbsp;<i className="fa fa-sign-out me-1"></i></Button>
               :
               <Button variant="warning" onClick={logIn}><i className="fa fa-user"></i> Ingresar</Button>
             }
@@ -38,7 +38,7 @@ const Menu = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
