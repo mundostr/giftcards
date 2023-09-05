035e64c1437411bd5d283780eb3a42d81ac2096f
Carlos Perren <idux.net@gmail.com>
2023-09-05 08:44:11 -0300

    Comentarios proceso login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,30 +9,36 @@ const Login = () => {
   const [alertError, setAlertError] = useState('')
   const [frm, setFrm] = useState({ email: '', password: '' })
   
+  // useEffect con array vacío, se ejecuta SOLO al montar
   useEffect(() => {
     (async () => {
     })();
 
     return () => {}
-  }, []) // Array vacío, se ejecuta SOLO al montar
+  }, [])
 
+  // Cada vez que hay un cambio de contenido en los campos del formulario,
+  // actualizamos el objeto frm
   const handleChange = (event) => {
     setFrm({...frm, [event.target.name]: event.target.value});
   };
 
   const handleSubmit = async (e) => {
+    // Evitamos que el navegador siga su secuencia habitual, manejaremos nosotros el formulario
     e.preventDefault()
 
+    // Generamos referencia al objeto de formulario en el DOM para chequear validez.
     const frmElement = e.currentTarget
-
     if (!frmElement.checkValidity()) {
       setAlertError('Por favor, complete los campos requeridos')
       frmElement.querySelector('input[type=email]').focus()
     } else {
+      // Aunque el HTML nos indique formulario válido, podemos agregar más controles aquí
       if (frm.password.length > 0 && frm.password.length < 6) {
         setAlertError('La clave debe tener entre 6 y 12 caracteres')
         frmElement.querySelector('input[type=password]').focus()
       } else {
+        // Si todo está bien, realizamos una solicitud POST a la API, enviando el contenido del formulario
         setAlertError('')
 
         const resultado = await fetch('https://rolling55ibackend-production.up.railway.app/api/users/login', {
@@ -42,10 +48,13 @@ const Login = () => {
         })
         const resultadoJson = await resultado.json();
 
+        // Si la API responde con un OK, quiere decir que la autenticación fue exitosa, disponemos de datos
+        // de usuario, incluyendo un token, los almacenamos en localStorage.
         if (resultadoJson.status == 'OK') {
           localStorage.setItem('cart_user', JSON.stringify(resultadoJson.data));
           navigate('/giftcards', { replace: true });
         } else {
+          // Si hubo algún problema, el objeto data contendrá el mensaje de error, lo mostramos y volvemos al formulario
           setAlertError(resultadoJson.data)
           frmElement.querySelector('input[type=email]').focus()
         }
@@ -66,6 +75,13 @@ const Login = () => {
 
           <Col className="col-xs-12  col-md-6 col-home">
             <Row className="card p-3" style={{width: '100%'}}>
+              {/*
+              noValidate evita que el navegador muestre los mensajes de validación por defecto en los campos,
+              los reemplazaremos por nuestro Alert con un mensaje personalizado para cada caso.
+
+              MUY IMPORTANTE que el name de los input (Form.Control) coincida con lo que el endpoint espera,
+              caso contrario hay que rearmar el objeto con las keys que corresponde más arriba en la solicitud POST
+              */}
               <Form noValidate onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
                   <Form.Label>Email</Form.Label>
@@ -79,28 +95,15 @@ const Login = () => {
                 
                 <Button type="submit" variant="warning">Ingresar</Button>
 
+                {/*
+                Esta caja de alerta solo aparece si alertError contiene algún texto
+                */}
                 {alertError && <Alert className="mt-3" key="warning" variant="warning">{alertError}</Alert>}
               </Form>
             </Row>
           </Col>
         </Row>
       </Container>
-
-      {/* <Container className="col col-md-4 offset-md 4 mt-4 p-4 bg-light container-blocks">
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3">
-            <Form.Label>Usuario</Form.Label>
-            <Form.Control type="text" placeholder="Nombre" value={frm.username} name="username" maxLength={16} required  autoFocus onChange={handleChange} />
-          </Form.Group>
-
-          <Form.Group className="mb-3">
-            <Form.Label>Clave</Form.Label>
-            <Form.Control type="password" value={frm.password} name="password" maxLength={12} required onChange={handleChange} />
-          </Form.Group>
-
-          <Button type="submit" variant="warning">Ingresar</Button>
-        </Form>
-      </Container> */}
     </>
   )
 }
